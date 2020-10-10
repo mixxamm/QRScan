68ff6ae43434174cd5f580f15f5f42c4de09a46f
mixxamm <maximjanssensj5@gmail.com>
2020-10-10 23:15:12 +0200

    Finalize scanning functionality

--- lib/qrview.dart
+++ lib/qrview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:screen/screen.dart';
 
 class QRViewExample extends StatefulWidget {
   const QRViewExample({
@@ -17,7 +18,7 @@ class _QRViewExampleState extends State<QRViewExample> {
   final GlobalKey qrKey = GlobalKey();
   bool flash = false;
   bool front = false;
-  double brightness = 0;
+  bool scanning = true;
 
   @override
   Widget build(BuildContext context) {
@@ -26,62 +27,93 @@ class _QRViewExampleState extends State<QRViewExample> {
         children: <Widget>[
           Expanded(
             flex: 4,
-            child: Stack(children: [
-              QRView(
-                key: qrKey,
-                onQRViewCreated: _onQRViewCreated,
-                overlay: QrScannerOverlayShape(
-                  borderColor: Colors.blue,
-                  borderRadius: 15,
-                  borderLength: 50,
-                  borderWidth: 3,
-                  cutOutSize: MediaQuery.of(context).size.height * 0.5,
-                  overlayColor:
-                      flash && front ? Colors.white70 : Colors.black45,
-                ),
+            child: AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              transitionBuilder: (Widget child, Animation<double> animation) =>
+                  ScaleTransition(
+                child: child,
+                scale: animation,
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            flash = !flash;
-                          });
-                          controller.toggleFlash();
-                        },
-                        icon: Icon(
-                          flash ? Icons.flash_on : Icons.flash_off,
-                          color: Colors.white,
-                          size: 38,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            front = !front;
-                          });
-                          controller.flipCamera();
-                        },
-                        icon: Icon(
-                          front ? MdiIcons.cameraFront : MdiIcons.cameraRear,
-                          color: Colors.white,
-                          size: 38,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ]),
+              child: scanning ? scanView() : resultView(),
+            ),
           ),
         ],
       ),
     );
+  }
+
+  Widget resultView() {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(qrText),
+        RaisedButton(
+          child: Text("Scan again"),
+          onPressed: () {
+            setState(() {
+              scanning = true;
+            });
+          },
+        )
+      ],
+    ));
+  }
+
+  Widget scanView() {
+    return Stack(children: [
+      QRView(
+        key: qrKey,
+        onQRViewCreated: _onQRViewCreated,
+        overlay: QrScannerOverlayShape(
+          borderColor: Colors.blue,
+          borderRadius: 15,
+          borderLength: 50,
+          borderWidth: 3,
+          cutOutSize: MediaQuery.of(context).size.height * 0.5,
+          overlayColor: flash && front ? Colors.white70 : Colors.black45,
+        ),
+      ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    flash = !flash;
+                  });
+                  controller.toggleFlash();
+                  _setBrightness();
+                },
+                icon: Icon(
+                  flash ? Icons.flash_on : Icons.flash_off,
+                  color: Colors.white,
+                  size: 38,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    front = !front;
+                  });
+                  controller.flipCamera();
+                  _setBrightness();
+                },
+                icon: Icon(
+                  front ? MdiIcons.cameraFront : MdiIcons.cameraRear,
+                  color: Colors.white,
+                  size: 38,
+                ),
+              )
+            ],
+          ),
+        ),
+      )
+    ]);
   }
 
   void _onQRViewCreated(QRViewController controller) {
@@ -90,8 +122,16 @@ class _QRViewExampleState extends State<QRViewExample> {
       controller.pauseCamera();
       setState(() {
         qrText = scanData;
+        scanning = false;
       });
     });
+  }
+
+  void _setBrightness() async {
+    if (front && flash)
+      Screen.setBrightness(1);
+    else
+      Screen.setBrightness(-1);
   }
 
   @override
